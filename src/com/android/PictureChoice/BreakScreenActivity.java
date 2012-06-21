003e8e0c3e89d7595441165c0912a0196d949196
Howon Lee <hlee.howon@gmail.com>
2012-06-21 13:12:50 -0700

    changed 'main' ui, allowed you to be done

--- src/com/android/PictureChoice/BreakScreenActivity.java
+++ src/com/android/PictureChoice/BreakScreenActivity.java
@@ -4,23 +4,36 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class BreakScreenActivity extends Activity {
-	
+	//maybe move this to the globals?
+	final int totalBlocks = 2;
+	int currentBlock;
 	Button toBlock;
+	TextView breakMsg;
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.breakscreen);
         toBlock = (Button) findViewById(R.id.button_to_block);
+        breakMsg = (TextView) findViewById(R.id.break_msg);
         toBlock.setOnClickListener(new OnClickListener(){
         	public void onClick(View view){
         		startActivity(new Intent("com.android.BLOCKSHOW"));
         	}
         });
+        currentBlock = GlobalVar.getInstance().getBlockNum();
+        if (currentBlock == totalBlocks){
+        	breakMsg.setText("@string/ok_done");
+        	toBlock.setOnClickListener(new OnClickListener(){
+        		public void onClick(View view){
+        			finish();
+        		}
+        	});
+        }
 	}
 
 }
