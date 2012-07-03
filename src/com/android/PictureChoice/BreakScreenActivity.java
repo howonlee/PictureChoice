eaf858332d68aeab3e08cc561bfd2512690f515f
Howon Lee <hlee.howon@gmail.com>
2012-07-03 16:25:48 -0700

    refactored imports

--- src/com/android/PictureChoice/BreakScreenActivity.java
+++ src/com/android/PictureChoice/BreakScreenActivity.java
@@ -1,10 +1,8 @@
 package com.android.PictureChoice;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PowerManager;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
